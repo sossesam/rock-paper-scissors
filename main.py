56e8bcc5972901273fe23e3467ec142b71394535
sossesam <sossesam@gmail.com>
2022-06-03 07:50:09 +0100

    rock paper scissors game working

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import random
+
+
 possible_options = ["R", "P","S"]
 
 def game_play():
-        comp = "S"
+        
 
         while 1 == 1:
-                user_choice = input("Select an option R for rock, P for paper, S for scissors : ")
+                comp = random.choice(possible_options)
+                user_choice = input("Select an option R for rock, P for paper, S for scissors : ").upper()
                 while user_choice not in possible_options:
                         print("you selected an invalid option")
-                        user_choice  = input("Select an option R for rock, P for paper, S for scissors : ")
+                        user_choice  = input("Select an option R for rock, P for paper, S for scissors : ").upper()
                         
 
                 if user_choice == comp:
@@ -19,6 +23,22 @@ def game_play():
                         else:
                                 print("You win. You rolled:{}, and computer rolled: {}".format(user_choice, comp))
                                 break
+                
+                elif user_choice == "P":
+                        if comp == "R":
+                                print("You win. You rolled:{}, and computer rolled: {}".format(user_choice, comp))
+                                break
+                        else:
+                                print("You loss. You rolled:{}, and computer rolled: {}".format(user_choice, comp))
+                
+                elif user_choice == "S":
+                        if comp == "P":
+                                print("You win. You rolled:{}, and computer rolled: {}".format(user_choice, comp))
+                                break
+                        else:
+                                print("You loss. You rolled:{}, and computer rolled: {}".format(user_choice, comp))
+                                
+                
 
                 
 
