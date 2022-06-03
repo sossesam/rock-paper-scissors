69d17b4e73d079f85a79f2b2a47eb26c4ec0fe49
sossesam <sossesam@gmail.com>
2022-06-03 07:18:24 +0100

    While loop and tie done

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 possible_options = ["R", "P","S"]
 
-print("hello word")
+def game_play():
+        comp = "R"
+
+        while 1 == 1:
+                user_choice = " "
+                while user_choice not in possible_options:
+                        user_choice = input("Select an option R for rock, P for paper, S for scissors : ")
+
+                if user_choice == comp:
+                        print("its a tie You rolled:{}, and computer rolled: {}".format(user_choice, comp))
+                
+                
+
+
+
+game_play()
